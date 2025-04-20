fb3f34f93ca092aee0eddbe245250e7726a577ca
frozzun <eonjun0783@naver.com>
2025-04-20 22:42:31 +0900

    Initial setting

---
+++ src/main/java/inu/unithon/backend/global/entity/BaseEntity.java
@@ -0,0 +1,22 @@
+package inu.unithon.backend.global.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
+
+  private boolean isDeleted;
+}
