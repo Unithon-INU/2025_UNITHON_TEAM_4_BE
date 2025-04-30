700c5da3b91738d9f9cb3fa7f6b40fa05a27b6d1
frozzun <eonjun0783@naver.com>
2025-04-30 13:52:29 +0900

    오류 수정

--- src/main/java/inu/unithon/backend/domain/member/dto/SignupRequestDto.java
+++ src/main/java/inu/unithon/backend/domain/member/dto/SignupRequestDto.java
@@ -5,9 +5,10 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Data
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignupRequestDto {
@@ -18,7 +19,6 @@ public class SignupRequestDto {
   private String profileImageUrl;
 
   @NotBlank(message = "이메일은 필수 입력 값입니다.")
-  @Email(message = "이메일 형식에 맞지 않습니다.")
   private String email;
 
   @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
