85a8bc914a6acbf548ce14b7658fe827996463cc
Youth-one <o3her456@gmail.com>
2025-05-14 16:21:32 +0900

    feat : 이중인코딩 문제 및 인코딩과정에서 오류 발생하여 직접 삽입으로 변경

--- src/main/java/inu/unithon/backend/domain/festival/service/FestivalService.java
+++ src/main/java/inu/unithon/backend/domain/festival/service/FestivalService.java
@@ -15,45 +15,42 @@ import org.slf4j.LoggerFactory;
 @RequiredArgsConstructor
 public class FestivalService {
 
-    private final RestTemplate restTemplate;
-    private static final Logger log = LoggerFactory.getLogger(FestivalService.class);
-
-
-    // application.yml 또는 application.properties에서 주입
     @Value("${tourapi.service-key}")
-    private String serviceKey;
+    private String encodedServiceKey; // 인코딩된 값 그대로
+
+    private final RestTemplate restTemplate;
 
     public FestivalResponseDto getFestivalList(String lang, String areaCode, String numOfRows, String pageNo) {
-        //tour api 를 활용하여 축제 리스트 불러오기 (변수값으로 지역코드, 페이지번호, 언어코드, 한페이지에 보여줄 갯수)
-        //일단 기본값으로 언어는 korean 지역은 서울로 정함 ㅇㅇ
-        String serviceName = getServiceName(lang);
-        // 상태관리로 넘겨온 언어코드를 조건문으로 걸어 어떤 언어로 api를 호출할것인지 체크하는 변수설정
 
-        UriComponentsBuilder builder = UriComponentsBuilder
-                .fromHttpUrl("http://apis.data.go.kr/B551011/" + serviceName + "/areaBasedList1")
-                .queryParam("serviceKey", serviceKey) // appconfig에서 설정하여 주입하여 넣는다 아마도 보안관련해서 더 좋을거 같다고 판단하여 넣었삼
-                .queryParam("MobileApp", "UnithonApp")
-                .queryParam("MobileOS", "ETC")
-                .queryParam("arrange", "A")
-                .queryParam("listYN", "Y")
-                .queryParam("_type", "json") // 모든 tour api 호출시 어떤 타입으로 받을지 설정해야 하는데 json으로 설정 가능
-                .queryParam("numOfRows", numOfRows)
-                .queryParam("pageNo", pageNo);
+        String serviceName = getServiceName(lang);
+
+        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B551011/");
+        urlBuilder.append(serviceName).append("/areaBasedList1")
+                .append("?serviceKey=").append(encodedServiceKey)
+                .append("&MobileApp=UnithonApp")
+                .append("&MobileOS=ETC")
+                .append("&arrange=A")
+                .append("&listYN=Y")
+                .append("&_type=json")
+                .append("&numOfRows=").append(numOfRows)
+                .append("&pageNo=").append(pageNo);
+        // queryparam 방식을 넣을라고 했는데 docodedServiceKey가 encodedServiceKey로 바뀌는데 그 과정에서 url
+        // 우리가 받은 encoded 된 키값과 다르게 인코딩 되어서 강제로 .append 로 수동조립 방식으로 했는데 이거 바꿔야하나여???????
 
         if (areaCode != null && !areaCode.isBlank()) {
-            builder.queryParam("areaCode", areaCode);
+            urlBuilder.append("&areaCode=").append(areaCode);
         }
 
-        String url = builder.toUriString();
-        log.info("📡 테스트 호출 URL: {}", url);
+        String finalUrl = urlBuilder.toString();
 
-        return restTemplate.getForObject(url, FestivalResponseDto.class);
+        // 테스트용ㅇㅇㅇ
+        System.out.println("📡 실제 요청 URL: " + finalUrl);
+
+        return restTemplate.getForObject(finalUrl, FestivalResponseDto.class);
     }
-
 
     private String getServiceName(String lang) {
         return switch (lang.toLowerCase()) {
-            //언어코드 조건문(추후 변경 가능성 있음)
             case "kor" -> "KorService1";
             case "jpn" -> "JpnService1";
             case "chn" -> "ChnService1";
