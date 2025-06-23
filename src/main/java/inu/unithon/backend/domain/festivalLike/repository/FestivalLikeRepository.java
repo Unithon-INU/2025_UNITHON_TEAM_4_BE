ea7f092b4b33098cc0b451e9ea545046d5c8acf5
Youth-one <o3her456@gmail.com>
2025-06-23 16:14:32 +0900

    fix : made repository

---
+++ src/main/java/inu/unithon/backend/domain/festivalLike/repository/FestivalLikeRepository.java
@@ -0,0 +1,16 @@
+package inu.unithon.backend.domain.festivalLike.repository;
+
+import inu.unithon.backend.domain.festivalLike.entity.FestivalLike;
+import inu.unithon.backend.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface FestivalLikeRepository extends JpaRepository<FestivalLike, Long> {
+    Optional<FestivalLike> findByMemberAndContentId(Member member, String contentId);
+    List<FestivalLike> findAllByMember(Member member);
+    void deleteByMemberAndContentId(Member member, String contentId);
+}
