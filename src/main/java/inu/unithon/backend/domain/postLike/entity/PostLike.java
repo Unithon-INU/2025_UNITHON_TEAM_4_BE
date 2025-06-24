9160416bc5c37e308d7d3938d645c83be24b5a59
Youth-one <o3her456@gmail.com>
2025-06-24 20:51:09 +0900

    fix : made entity

--- src/main/java/inu/unithon/backend/domain/postLike/entity/PostLike.java
+++ src/main/java/inu/unithon/backend/domain/postLike/entity/PostLike.java
@@ -1,4 +1,24 @@
 package inu.unithon.backend.domain.postLike.entity;
 
+import inu.unithon.backend.domain.member.entity.Member;
+import inu.unithon.backend.domain.post.entity.Post;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class PostLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
 }
---
+++ src/main/java/inu/unithon/backend/domain/postLike/repository/PostLikeRepository.java
@@ -0,0 +1,4 @@
+package inu.unithon.backend.domain.postLike.repository;
+
+public interface PostLikeRepository {
+}
