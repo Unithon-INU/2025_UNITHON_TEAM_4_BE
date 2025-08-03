a1727b293a3fc30547f70f1605f1610d29c6a504
Youth-one <o3her456@gmail.com>
2025-08-03 16:05:22 +0900

    feat : rabbitMq init setting (fixing .env file)

--- src/main/java/inu/unithon/backend/domain/festival/repository/FestivalRepository.java
+++ src/main/java/inu/unithon/backend/domain/festival/repository/FestivalRepository.java
@@ -3,11 +3,14 @@ package inu.unithon.backend.domain.festival.repository;
 import inu.unithon.backend.domain.festival.entity.Festival;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.*;
 
 public interface FestivalRepository extends JpaRepository<Festival, Long>{
-    boolean existsByContentId(long contentId);
-    @Query("SELECT f.contentId FROM Festival f WHERE f.contentId IN :contentId")
-    List<Long> findContentIdsByContentIds(List<Long> contentIds);
+//    boolean existsByContentId(long contentId);
+
+    @Query("SELECT f.contentId FROM Festival f WHERE f.contentId IN :contentIds")
+    List<Long> findContentIdsByContentIds(@Param("contentId") List<Long> contentIds);
 
 }
